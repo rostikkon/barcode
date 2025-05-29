fc7feaa2bc9b4a8b577127675a2a3e3604d038b9
ROSTIK <rostikkon07@gmail.com>
2025-05-29 20:54:22 +0200

    - Zmena ve tridach Code39 a Code128 (dodani metod pro nacitani hodnot do hashmapy ze souboru)
     - Pridani souboru se vzorky pro sifrovani

--- src/Code39.java
+++ src/Code39.java
@@ -1,57 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 
 public class Code39 extends CarovyKod {
-    private static final HashMap<Character, String> MAPA_CODE39 = new HashMap<>();
 
-    static {
-        MAPA_CODE39.put('0', "101001101101");
-        MAPA_CODE39.put('1', "110100101011");
-        MAPA_CODE39.put('2', "101100101011");
-        MAPA_CODE39.put('3', "110110010101");
-        MAPA_CODE39.put('4', "101001101011");
-        MAPA_CODE39.put('5', "110100110101");
-        MAPA_CODE39.put('6', "101100110101");
-        MAPA_CODE39.put('7', "101001011011");
-        MAPA_CODE39.put('8', "110100101101");
-        MAPA_CODE39.put('9', "101100101101");
-        MAPA_CODE39.put('A', "110101001011");
-        MAPA_CODE39.put('B', "101101001011");
-        MAPA_CODE39.put('C', "110110100101");
-        MAPA_CODE39.put('D', "101011001011");
-        MAPA_CODE39.put('E', "110101100101");
-        MAPA_CODE39.put('F', "101101100101");
-        MAPA_CODE39.put('G', "101010011011");
-        MAPA_CODE39.put('H', "110101001101");
-        MAPA_CODE39.put('I', "101101001101");
-        MAPA_CODE39.put('J', "101011001101");
-        MAPA_CODE39.put('K', "110101010011");
-        MAPA_CODE39.put('L', "101101010011");
-        MAPA_CODE39.put('M', "110110101001");
-        MAPA_CODE39.put('N', "101011010011");
-        MAPA_CODE39.put('O', "110101101001");
-        MAPA_CODE39.put('P', "101101101001");
-        MAPA_CODE39.put('Q', "101010110011");
-        MAPA_CODE39.put('R', "110101011001");
-        MAPA_CODE39.put('S', "101101011001");
-        MAPA_CODE39.put('T', "101011011001");
-        MAPA_CODE39.put('U', "110010101011");
-        MAPA_CODE39.put('V', "100110101011");
-        MAPA_CODE39.put('W', "110011010101");
-        MAPA_CODE39.put('X', "100101101011");
-        MAPA_CODE39.put('Y', "110010110101");
-        MAPA_CODE39.put('Z', "100110110101");
-        MAPA_CODE39.put('-', "100101011011");
-        MAPA_CODE39.put('.', "110010101101");
-        MAPA_CODE39.put(' ', "100110101101");
-        MAPA_CODE39.put('$', "100100100101");
-        MAPA_CODE39.put('/', "100100101001");
-        MAPA_CODE39.put('+', "100101001001");
-        MAPA_CODE39.put('%', "101001001001");
-        MAPA_CODE39.put('*', "100101101101");
+    private static final HashMap<Character, String> MAPA_CODE39 = new HashMap<>();
+    private static boolean mapaInicializovana = false;
+
+    private static void inicializujMapu() {
+        if (mapaInicializovana) {
+            return;
+        }
+        try (BufferedReader reader = new BufferedReader(new FileReader("Soubory/code39.txt"))) {
+            String radek;
+            while ((radek = reader.readLine()) != null) {
+
+                String[] casti = radek.split("=");
+                char znak = casti[0].charAt(0);
+                String vzor = casti[1].trim();
+                MAPA_CODE39.put(znak, vzor);
+            }
+            mapaInicializovana = true;
+        } catch (IOException e) {
+            throw new RuntimeException("Chyba pri nacitani souboru code39.txt: " + e.getMessage());
+        }
     }
 
     @Override
     public String zakoduj(String vstup) {
+        inicializujMapu();
         if (vstup == null || vstup.isEmpty()) {
             throw new IllegalArgumentException("Vstup nesmi byt prazdny!");
         }
--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,11 @@ public class Main {
     public static void main(String[] args) {
         try {
             Code39 code39 = new Code39();
-            code39.vytvorObrazek("FDS12345", "code39.png");
+            code39.vytvorObrazek("FDS 12345", "code39.png");
             System.out.println("Code39 vygenerovan!");
+            Code128 code128 = new Code128();
+            code128.vytvorObrazek("FDS 12345", "code128.png");
+            System.out.println("Code128 vygenerovan!");
         } catch (IOException e) {
             System.out.println("Chyba pri ukladani obrazku: " + e.getMessage());
         } catch (IllegalArgumentException e) {
